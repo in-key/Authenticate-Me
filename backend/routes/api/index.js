bfb37eecfdd429c3f179b63e71cce8a2a0536080
in-key <inkeydev@gmail.com>
2023-01-04 16:44:57 -0500

    add logout

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,5 +1,9 @@
 const router = require('express').Router();
+const sessionRouter = require('./session');
+const usersRouter = require('./users');
 
+router.use('/session', sessionRouter);
+router.use('/users', usersRouter);
 
 router.post('/test', function(req, res) {
     res.json({ requestBody: req.body });
